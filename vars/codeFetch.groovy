867484c16e9d657d462a86bf037d5856c56a1067
checkacer <44667785+checkacer@users.noreply.github.com>
2019-03-05 15:52:23 +0800

    Update codeFetch.groovy

--- vars/codeFetch.groovy
+++ vars/codeFetch.groovy
@@ -1,1 +1,19 @@
-def call() { echo "starting fetch code......" }
+#!groovy
+def call(){
+   pipeline{
+     agent any
+     def mvnHome
+     def jdkHome
+     stage('Preparation') {
+       git credentialsId: '13735461-01df-48bf-85cc-373338e73227', url: 'https://github.com/checkacer/runindockerdemo' 
+       mvnHome = tool 'maven'
+       jdkHome = tool 'jdk'
+     }
+     stage('Package') {
+       sh "'${mvnHome}/bin/mvn' package"
+     }
+     stage('Results') {
+     archive 'target/*.jar'
+     }
+   }
+}
